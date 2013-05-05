e963ec7fbf8d0a4a094fb7b8652c5d41d75c00d8
Raghu Betina <rbetina@gmail.com>
2013-05-05 18:59:23 -0500

    Added vote resource.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130505233059) do
+ActiveRecord::Schema.define(:version => 20130505235909) do
 
   create_table "actors", :force => true do |t|
     t.string "name"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(:version => 20130505233059) do
   create_table "users", :force => true do |t|
     t.string "username"
     t.string "password_digest"
+  end
+
+  create_table "votes", :force => true do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
   end
 
 end
