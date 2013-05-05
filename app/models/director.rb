1671692db127d941af37d45b6e0294540f9b735e
Raghu Betina <rbetina@gmail.com>
2013-05-05 18:20:42 -0500

    Added model association methods.

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,3 @@
 class Director < ActiveRecord::Base
+  has_many :movies
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  belongs_to :director
+  has_many :roles
+  has_many :actors, through: :roles
 end
