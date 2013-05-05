3e53a4828479f919ce7686c22b14e39519607b80
Raghu Betina <rbetina@gmail.com>
2013-05-05 18:53:15 -0500

    Added sign-in logic.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_username(params[:username])
+
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Signed in successfully."
+    else
+      redirect_to new_session_url, notice: "Something went wrong."
+    end
   end
 
   def destroy
