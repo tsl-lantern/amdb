d6b40c133b77e80c719139263213af064600dd89
Raghu Betina <rbetina@gmail.com>
2013-05-06 08:12:01 -0500

    Validate presence of character name for Role.

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,7 @@ class Role < ActiveRecord::Base
   belongs_to :movie
   belongs_to :actor
 
+  validates :character_name, presence: true
   validates :movie, presence: true
   validates :actor, presence: true
 end
