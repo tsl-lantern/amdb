96e066948628021c85e8ca0d40635487c98e61fc
Raghu Betina <rbetina@gmail.com>
2013-05-05 19:11:48 -0500

    Started using cookies and filters.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,13 @@
 class VotesController < ApplicationController
 
+  before_filter :require_sign_in
+
+  def require_sign_in
+    if session[:user_id].blank?
+      redirect_to :back, notice: "Must be signed in to vote."
+    end
+  end
+
   def index
     @votes = Vote.all
   end
@@ -16,7 +24,7 @@ class VotesController < ApplicationController
     @vote = Vote.new
     @vote.user_id = params[:user_id]
     @vote.movie_id = params[:movie_id]
-    
+
     if @vote.save
             redirect_to votes_url
           else
@@ -26,13 +34,21 @@ class VotesController < ApplicationController
 
   def edit
     @vote = Vote.find_by_id(params[:id])
+    if @vote.user_id != session[:user_id]
+      redirect_to :back, notice: "Nice try."
+      return
+    end
   end
 
   def update
     @vote = Vote.find_by_id(params[:id])
+    if @vote.user_id != session[:user_id]
+      redirect_to :back, notice: "Nice try."
+      return
+    end
     @vote.user_id = params[:user_id]
     @vote.movie_id = params[:movie_id]
-    
+
     if @vote.save
             redirect_to votes_url
           else
@@ -42,6 +58,10 @@ class VotesController < ApplicationController
 
   def destroy
     @vote = Vote.find_by_id(params[:id])
+    if @vote.user_id != session[:user_id]
+      redirect_to :back, notice: "Nice try."
+      return
+    end
     @vote.destroy
         redirect_to votes_url
       end
