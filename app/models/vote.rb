6e14d742497f57101e660465c77bb0d9f92b4772
Raghu Betina <rbetina@gmail.com>
2013-05-05 19:26:05 -0500

    Added advanced validations.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,12 @@ class Vote < ActiveRecord::Base
 
   validates :user, presence: true
   validates :movie, presence: true
+  validates :user_id, uniqueness: { scope: :movie_id }
+  validate :user_cannot_have_more_than_three_votes
+
+  def user_cannot_have_more_than_three_votes
+    if self.user.votes.count >= 3
+      self.errors.add(:user, "cannot vote more than three times")
+    end
+  end
 end
